c0f6a47a9b2e4ca914c78854d33d3ca1f7e0c2b1
Parag Baxi <ghee22@gmail.com>
2014-05-29 22:24:24 -0700

    handle concurrent scans
    concurrent_scans_retry_delay & concurrent_scans_retries

--- qualysguard_scan_queue.py
+++ qualysguard_scan_queue.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 '''Run multiple WAS v2 scans as a workaround to the concurrency limit.
 
@@ -11,17 +11,20 @@ Add ability to import CSV.
 '''
 
 import argparse
-import base64, urllib2
-import datetime, time
+import base64
+import urllib2
+import datetime
+import time
 import logging
 import os
-import qualysapi
 import random
 import unicodedata
-
 from collections import defaultdict
+
+import qualysapi
 from lxml import objectify
 from texttable import Texttable
+
 
 def list_apps(apps):
     """Print applications into a pretty table.
@@ -29,10 +32,10 @@ def list_apps(apps):
     table = Texttable()
     table.set_cols_align(["r", "l", "l"])
     table.set_cols_valign(["m", "m", "m"])
-    table.add_rows([ ['App #', 'App name', 'App ID #'], ], header = True) 
-    c=0
+    table.add_rows([['App #', 'App name', 'App ID #'], ], header=True)
+    c = 0
     for webapp in apps:
-        c+=1
+        c += 1
         table.add_row([c, webapp['name'], webapp['id']])
     # Print table.
     print (table.draw() + '\n')
@@ -40,33 +43,37 @@ def list_apps(apps):
 
 # Start of script.
 # Declare the command line flags/options we want to allow.
-parser = argparse.ArgumentParser(description = 'Automate concurrent scanning of multiple QualysGuard webapps.')
-parser.add_argument('-a', '--all_apps', action = 'store_true',
-                    help = 'Select all web applications. Overwrites any tag filters.')
-parser.add_argument('-c', '--concurrency_limit', default = 10,
-                    help = 'Limit scans to CONCURRENCY_LIMIT simultaneous scans. (Default = 10)')
+parser = argparse.ArgumentParser(description='Automate concurrent scanning of multiple QualysGuard webapps.')
+parser.add_argument('-a', '--all_apps', action='store_true',
+                    help='Select all web applications. Overwrites any tag filters.')
+parser.add_argument('-c', '--concurrency_limit', default=10,
+                    help='Limit scans to CONCURRENCY_LIMIT simultaneous scans. (Default = 10)')
+parser.add_argument('--concurrent_scans_retries', default=0,
+                    help='Retry the call this many times if your subscription hits the concurrent scans limit. (Default = 0)')
+parser.add_argument('--concurrent_scans_retry_delay', default=0,
+                    help='Delay in seconds between retrying when subscription hits the concurrent scans limit. (Default = 0)')
 parser.add_argument('--config',
-                    help = 'Configuration for Qualys connector.')
-parser.add_argument('-d', '--delay', default = 60,
-                    help = 'Wait DELAY seconds between scan attempts if concurrency limit is reached. (Default = 60)')
-parser.add_argument('--debug', action = 'store_true',
-                    help = 'Outputs additional information to log.')
-parser.add_argument('-f', '--file', default = 'apps.txt',
-                    help = 'Output file to store XML results from initiating scans. (Default = apps.txt)')
-parser.add_argument('-l', '--no_list', action = 'store_true', default = False,
-                    help = 'Do not list all selected web applications. (Default = False)')
+                    help='Configuration for Qualys connector.')
+parser.add_argument('-d', '--delay', default=60,
+                    help='Wait DELAY seconds between scan attempts if concurrency limit is reached. (Default = 60)')
+parser.add_argument('--debug', action='store_true',
+                    help='Outputs additional information to log.')
+parser.add_argument('-f', '--file', default='apps.txt',
+                    help='Output file to store XML results from initiating scans. (Default = apps.txt)')
+parser.add_argument('-l', '--no_list', action='store_true', default=False,
+                    help='Do not list all selected web applications. (Default = False)')
 parser.add_argument('-o', '--option_profile',
-                    help = 'Optionally override default option profile to scan selected web applications with OPTION_PROFILE ID.')
-parser.add_argument('-r', '--randomize', action = 'store_true',
-                    help = 'Randomize scanning of web applications.')
-parser.add_argument('-s', '--scan', action = 'store_true',
-                    help = 'Scan all selected web applications.')
+                    help='Optionally override default option profile to scan selected web applications with OPTION_PROFILE ID.')
+parser.add_argument('-r', '--randomize', action='store_true',
+                    help='Randomize scanning of web applications.')
+parser.add_argument('-s', '--scan', action='store_true',
+                    help='Scan all selected web applications.')
 parser.add_argument('-t', '--tag',
-                    help = 'Filter selection of web applications to those with TAG.')
+                    help='Filter selection of web applications to those with TAG.')
 #parser.add_argument('-T', '--Tags_file',
 #                    help = 'Filter selection of web applications to those with all tags from TAGS_FILE (tags combined using a logical AND). Tags from file should be separated by line break.')
-parser.add_argument('-y', '--scan_type', default = 'discovery',
-                    help = 'Scan type: discovery, vulnerability. (Default = discovery)')
+parser.add_argument('-y', '--scan_type', default='discovery',
+                    help='Scan type: discovery, vulnerability. (Default = discovery)')
 # Parse arguments.
 c_args = parser.parse_args()
 # Create log directory.
@@ -82,11 +89,11 @@ LOG_FILENAME = '%s/%s-%s.log' % (PATH_LOG,
 if c_args.debug:
     # Enable debug level of logging.
     print "Logging level set to debug."
-    logging.basicConfig(filename = LOG_FILENAME, format = '%(asctime)s %(message)s',
-                        level = logging.DEBUG)
+    logging.basicConfig(filename=LOG_FILENAME, format='%(asctime)s %(message)s',
+                        level=logging.DEBUG)
 else:
-    logging.basicConfig(filename = LOG_FILENAME, format = '%(asctime)s %(message)s',
-                        level = logging.INFO)
+    logging.basicConfig(filename=LOG_FILENAME, format='%(asctime)s %(message)s',
+                        level=logging.INFO)
 # Validate arguements.
 if not ((c_args.all_apps or c_args.tag or c_args.Tags_file)):
     parser.print_help()
@@ -136,7 +143,7 @@ while True:
         # App name may be in unicode.
         if isinstance(app_name, unicode):
             # Decode to string.
-            app_name = unicodedata.normalize('NFKD', app_name).encode('ascii','ignore')
+            app_name = unicodedata.normalize('NFKD', app_name).encode('ascii', 'ignore')
         app['name'] = app_name
         app['id'] = webapp.id.text
         apps_to_scan.append(app)
@@ -155,8 +162,8 @@ if not c_args.scan:
 if c_args.randomize:
     random.shuffle(apps_to_scan)
 # Start scanning.
-apps_scanned=[]
-logging.debug('Writing results of initiating app scans to %s' %(c_args.file))
+apps_scanned = []
+logging.debug('Writing results of initiating app scans to %s' % (c_args.file))
 f = file(c_args.file, 'w')
 for app in apps_to_scan:
     logging.debug('Attempting to scan %s, %s' % (app['name'], app['id']))
@@ -206,7 +213,7 @@ for app in apps_to_scan:
             # Under concurrency limit, do not delay.
             logging.debug('Running scan.')
             break
-        # Scan web app.
+    # Scan web app.
     query_uri = '/launch/was/wasscan'
     # Setup request to scan web app.
     scan_type = c_args.scan_type.upper()
@@ -248,7 +255,7 @@ for app in apps_to_scan:
     # Make request
     logging.info('Scanning %s (web app ID %s)...' % (app['name'], app['id']))
     print 'Scanning %s (web app ID %s)...' % (app['name'], app['id'])
-    response = qgc.request(query_uri, data)
+    response = qgc.request(query_uri, data, concurrent_scans_retries=c_args.concurrent_scans_retries, concurrent_scans_retry_delay=c_args.concurrent_scans_retry_delay)
     print 'Done:'
     logging.info(response)
     print response + '\n'
